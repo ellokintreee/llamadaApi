991ed422caf1dfd8192d5d98ce587c72cb4a5ed7
Usuario <danielakarinaceballes@gmail.com>
2023-05-01 07:32:30 -0300

    hay que hacer que deje de crear varios pokemones a la vez

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+const main = document.querySelector("main")
+const submit = document.querySelector("button")
+const input = document.querySelector("input")
+    submit.addEventListener("click", (e)=>{
+        e.preventDefault()
+        pokemon = input.value
+        elegirPke(pokemon)
+        
+    })
+    
+    
+    
+    function elegirPke() {
+        let buscarApi = fetch( `https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+        .then(res =>res.json())
+        .then(data =>{
+            crearPokemon(data)
+        })
+    }
+
+    function crearPokemon(pokemon) {
+        let newImage = document.createElement("img")
+        newImage.setAttribute("src",`${pokemon.sprites.front_default}` )
+        let txt = document.createElement("h2");
+        txt.textContent = `${pokemon.name} `
+        main.appendChild(newImage)
+        main.appendChild(txt)
+    }
+
