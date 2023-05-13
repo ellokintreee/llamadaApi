5ed584ca87f4757f3f713a12b7fa5cefa6c32416
Usuario <junjse4@gmail.com>
2023-05-13 04:41:59 -0300

    iniciamos el proyecto y definimos las primeras partes

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+/*vamos a realizar un organizador de tareas 
+para elo necesitamos.
+1-un  input donde ingresamos los datos
+2- hacer que esos datos queden guardados, en un li
+3-debemos dar la capacidad de eliminar esa tarea una ves realizada.*/
+const dato = document.querySelector("#datos");
+const agregar = document.querySelector("#agregar")
+const quitar = document.querySelector("#eliminar")
+const main = document.querySelector("main")
 
- function getCharacter (done) {
-    const results = fetch("https://rickandmortyapi.com/api/character")
-    results
-    .then(response => response.json())
-    .then(data => {
-        done(data)
-    })
- }
 
- getCharacter(data => {
-    data.results.forEach(personaje => {
-        const article = document.createRange().createContextualFragment(
-`
-          <article>
-            <div class="image-content">
-                <img src="${personaje.image}"  alt="${personaje.name}">
-            </div>
-            <h2>${personaje.name} </h2>
-            <span>${personaje.status} </span>
-        </article> `);
-            const main = document.querySelector("main");
-        main.appendChild(article)
-    });
- })
+
+/*quitar.addEventListener("click",(e)=>{
+    e.preventDefault()
+    task(removeChildNode)
+    
+})*/
+agregar.addEventListener("click",(e)=>{
+    e.preventDefault()
+    task(dato.value)
+})
+
+function task(id) {
+    let text = document.createElement("span")
+    text.textContent = `${id}`
+    text.classList.add("texto")
+    let btn =document.createElement("button",)
+    btn.textContent = "eliminar"
+    btn.classList.add("btn")
+    main.appendChild(text)
+    main.appendChild(btn)
+  
+}
+function removeChildNode(parent){
+while(parent.firstChild){
+    parent.removeChildNode(parent.firstChild)
+}
+}
